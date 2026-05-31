89f0fc07594d2225b1c88344d774a07970a36c3b
Giulia Compagnucci <giulia.compagnucci2003@gmail.com>
2026-05-31 20:53:51 +0200

    aggiunto dipendenze e primi avviamenti main

---
+++ code/src/main/java/it.unicam.cs.mpgc.rpg123420/MainApp.java
@@ -0,0 +1,18 @@
+package it.unicam.cs.mpgc.rpg123420;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("RPG Project - Matricola 123420");
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
